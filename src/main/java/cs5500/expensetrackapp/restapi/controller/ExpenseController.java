a3602712a23118f5acd0368b7f3f0cac4eba3dfb
Shizhan Liu <87243352+ShizhanLiu@users.noreply.github.com>
2025-03-25 22:40:10 -0400

    add the cors annotation

--- src/main/java/cs5500/expensetrackapp/restapi/controller/ExpenseController.java
+++ src/main/java/cs5500/expensetrackapp/restapi/controller/ExpenseController.java
@@ -9,12 +9,14 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 /**
  *
  * This is controller class for Expense module
