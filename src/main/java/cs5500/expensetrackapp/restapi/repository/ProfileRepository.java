6458401541dc926921bfe00f0219ec9e03c25faf
Shizhan Liu <87243352+ShizhanLiu@users.noreply.github.com>
2025-04-23 15:16:23 +0800

    Revert "Merge pull request #6 from ShizhanLiu/feature/user-register"
    This reverts commit 8f0386a07d4d47d3968065f82664c1aba3b7d5cf, reversing
    changes made to beb329198a0a94ef7024dff786d341cf5242c435.

--- src/main/java/cs5500/expensetrackapp/restapi/repository/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package cs5500.expensetrackapp.restapi.repository;
-
-import cs5500.expensetrackapp.restapi.entity.ProfileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository <ProfileEntity, Long> {
-}
