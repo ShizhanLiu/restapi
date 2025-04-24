9de7d97d7c1f33f9bc287a626f5c5f41a22d6523
dix <zhao.dix@northeastern.edu>
2025-04-23 18:49:47 -0700

    register bug

--- src/main/java/cs5500/expensetrackapp/restapi/controller/AuthController.java
+++ src/main/java/cs5500/expensetrackapp/restapi/controller/AuthController.java
@@ -1,60 +1,4 @@
-package cs5500.expensetrackapp.restapi.controller;//package cs5500.expensetrackapp.restapi.controller;
-//
-//import cs5500.expensetrackapp.restapi.dto.ProfileDTO;
-//import cs5500.expensetrackapp.restapi.io.ProfileRequest;
-//import cs5500.expensetrackapp.restapi.io.ProfileResponse;
-//import cs5500.expensetrackapp.restapi.service.ProfileService;
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.modelmapper.ModelMapper;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@Slf4j
-//@RequiredArgsConstructor
-//@RequestMapping("/api/v1")
-//@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
-//
-//public class AuthController {
-//
-//    private final ModelMapper modelMapper;
-//    private final ProfileService profileService;
-//
-//    /**
-//     * API endpoint to register new user
-//     * @param profileRequest
-//     * @return profileResponse
-//     */
-//
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @PostMapping("/register")
-//    public ProfileResponse createProfile(@Valid @RequestBody ProfileRequest profileRequest){
-//        log.info("API /register is called {}", profileRequest);
-//        ProfileDTO profileDTO = mapToProfileDTO(profileRequest);
-//        profileDTO = profileService.createProfile(profileDTO);
-//        log.info("Printing the profile dto details {}", profileDTO);
-//        return mapToProfileResponse(profileDTO);
-//    }
-//
-//    /**
-//     * Mapper method to map values from profile request to profile dto
-//     * @param profileRequest
-//     * @return profileDto
-//     */
-//    private ProfileDTO mapToProfileDTO(ProfileRequest profileRequest) {
-//        return modelMapper.map(profileRequest, ProfileDTO.class);
-//    }
-//    /**
-//     * Mapper method to map values from profile dto to profile response
-//     * @param profileDTO
-//     * @return profileResponse
-//     */
-//    private ProfileResponse mapToProfileResponse(ProfileDTO profileDTO) {
-//        return modelMapper.map(profileDTO, ProfileResponse.class);
-//    }
-//}
+package cs5500.expensetrackapp.restapi.controller;
 
 import cs5500.expensetrackapp.restapi.dto.ProfileDTO;
 import cs5500.expensetrackapp.restapi.io.ProfileRequest;
@@ -62,6 +6,7 @@ import cs5500.expensetrackapp.restapi.io.ProfileResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import cs5500.expensetrackapp.restapi.service.ProfileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.modelmapper.ModelMapper;
@@ -77,15 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final ModelMapper modelMapper;
+    private final ProfileService profileService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/register")
     public ProfileResponse createProfile(@Valid @RequestBody ProfileRequest profileRequest){
         log.info("API /register is called {}", profileRequest);
         ProfileDTO profileDTO = mapToProfileDTO(profileRequest);
+        profileDTO = profileService.createProfile(profileDTO);
+        log.info("Printing the profile dto details {}", profileDTO);
         return mapToProfileResponse(profileDTO);
     }
-
+    @ResponseStatus(HttpStatus.CREATED)
     private ProfileDTO mapToProfileDTO(ProfileRequest profileRequest) {
         return modelMapper.map(profileRequest, ProfileDTO.class);
     }
